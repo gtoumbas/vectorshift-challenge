590bc19955dbf38dccc843b88598189a8f0f860a
gtoumbas <70962697+gtoumbas@users.noreply.github.com>
2024-05-15 18:07:31 -0400

    added log flag to Go Factorizer

--- prime_factorizer/main.go
+++ prime_factorizer/main.go
@@ -2,44 +2,56 @@ package main
 
 import (
     "context"
+    "flag"
     "fmt"
     "log"
     "math/big"
     "os"
     "os/signal"
+    "strings"
     "syscall"
-	"strings"
 
     "github.com/segmentio/kafka-go"
 )
 
 func ConvertFactorsToString(factors []*big.Int) string {
-	// Convert each factor to a string and join them
-	factorStrs := make([]string, len(factors))
-	for idx, fac := range factors {
-		factorStrs[idx] = fac.String()
-	}
-	result := fmt.Sprintf("[%s]", strings.Join(factorStrs, " "))
-	return result
+    // Convert each factor to a string and join them
+    factorStrs := make([]string, len(factors))
+    for idx, fac := range factors {
+        factorStrs[idx] = fac.String()
+    }
+    result := fmt.Sprintf("[%s]", strings.Join(factorStrs, " "))
+    return result
 }
 
 func main() {
+    logFlag := flag.Bool("log", false, "Enable logging")
+    flag.Parse()
+
     // Kafka reader configuration
-    fmt.Println("Configuring Kafka reader...")
+    if *logFlag {
+        fmt.Println("Configuring Kafka reader...")
+    }
     r := kafka.NewReader(kafka.ReaderConfig{
         Brokers: []string{"localhost:9092"},
         Topic:   "numbers",
         GroupID: "prime-factorizer",
     })
-    fmt.Println("Kafka reader configured.")
+    if *logFlag {
+        fmt.Println("Kafka reader configured.")
+    }
 
     // Kafka writer configuration
-    fmt.Println("Configuring Kafka writer...")
+    if *logFlag {
+        fmt.Println("Configuring Kafka writer...")
+    }
     w := kafka.NewWriter(kafka.WriterConfig{
         Brokers: []string{"localhost:9092"},
         Topic:   "results",
     })
-    fmt.Println("Kafka writer configured.")
+    if *logFlag {
+        fmt.Println("Kafka writer configured.")
+    }
 
     ctx, cancel := context.WithCancel(context.Background())
     defer cancel()
@@ -49,36 +61,49 @@ func main() {
         sigchan := make(chan os.Signal, 1)
         signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
         <-sigchan
-        fmt.Println("Received shutdown signal, cancelling context...")
+        if *logFlag {
+            fmt.Println("Received shutdown signal, cancelling context...")
+        }
         cancel()
     }()
 
-	for {
-		fmt.Println("Listening for messages...")
+    for {
+        if *logFlag {
+            fmt.Println("Listening for messages...")
+        }
 
-		m, err := r.ReadMessage(ctx)
-		if err != nil {
-			log.Printf("could not read message: %v", err)
-			break
-		}
-		fmt.Printf("Message read: %s\n", string(m.Value))
-		identifier := string(m.Headers[0].Value)
-		number := new(big.Int)
-		number.SetString(string(m.Value), 10)
-		originalNumber := number.String()
-		fmt.Printf("Computing prime factors for number: %s\n", number.String())
-		factors := PrimeFactors(number)
+        m, err := r.ReadMessage(ctx)
+        if err != nil {
+            log.Printf("could not read message: %v", err)
+            break
+        }
 
-		result := ConvertFactorsToString(factors)
-		result = fmt.Sprintf("%s : %s", originalNumber, result)
+        if *logFlag {
+            fmt.Println("----------------------------\n")
+            fmt.Printf("Message read: %s\n", string(m.Value))
+        }
+        identifier := string(m.Headers[0].Value)
+        number := new(big.Int)
+        number.SetString(string(m.Value), 10)
+        originalNumber := number.String()
+        if *logFlag {
+            fmt.Printf("Computing prime factors for number: %s\n", number.String())
+        }
+        factors := PrimeFactors(number)
 
-		fmt.Printf("Debug: Writing message to Kafka with identifier %s and result %s\n", identifier, result)
-		err = w.WriteMessages(ctx, kafka.Message{
-			Value: []byte(result),
-			Headers: []kafka.Header{{Key: "ws-identifier", Value: []byte(identifier)}},
-		})
-		if err != nil {
-			log.Printf("could not write message: %v", err)
-		}
-	}
+        result := ConvertFactorsToString(factors)
+        result = fmt.Sprintf("%s : %s", originalNumber, result)
+
+        if *logFlag {
+            fmt.Printf("Writing message to Kafka with identifier %s and result %s\n\n", identifier, result)
+			fmt.Println("----------------------------")
+        }
+        err = w.WriteMessages(ctx, kafka.Message{
+            Value: []byte(result),
+            Headers: []kafka.Header{{Key: "ws-identifier", Value: []byte(identifier)}},
+        })
+        if err != nil {
+            log.Printf("could not write message: %v", err)
+        }
+    }
 }
